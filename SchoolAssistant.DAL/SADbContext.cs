60a2b542a4a0bc7c09c32685e1471705447ec25c
Dariusz Wiktorowski <dariusz.wiktorowski@investit.dev>
2022-03-26 00:33:12 +0100

    Add project files.

---
+++ SchoolAssistant.DAL/SADbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace SchoolAssistantWeb.Data
+{
+    public class SADbContext : IdentityDbContext
+    {
+        public SADbContext(DbContextOptions<SADbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
