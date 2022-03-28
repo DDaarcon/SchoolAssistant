ef74027244c39f1897c163859adab21f7db30db6
Dariusz Wiktorowski <dariusz.wiktorowski@investit.dev>
2022-03-28 20:04:16 +0200

    Periodicity of lessons saved in cron expression

--- SchoolAssistant.DAL/Models/Lessons/Lesson.cs
+++ SchoolAssistant.DAL/Models/Lessons/Lesson.cs
@@ -1,18 +1,16 @@
-﻿using SchoolAssistant.DAL.Models.Shared;
+﻿using SchoolAssistant.DAL.Models.Rooms;
+using SchoolAssistant.DAL.Models.Shared;
 using SchoolAssistant.DAL.Models.Staff;
 using SchoolAssistant.DAL.Models.StudentsOrganization;
 using SchoolAssistant.DAL.Models.Subjects;
-using System;
-using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations.Schema;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace SchoolAssistant.DAL.Models.Lessons
 {
     public class Lesson : SemesterDbEntity
     {
+        public string CronPeriodicity { get; set; }
+
         public long SubjectId { get; set; }
         [ForeignKey(nameof(SubjectId))]
         public virtual Subject Subject { get; set; } = null!;
@@ -27,5 +25,9 @@ namespace SchoolAssistant.DAL.Models.Lessons
         public long LecturerId { get; set; }
         [ForeignKey(nameof(LecturerId))]
         public virtual Teacher Lecturer { get; set; } = null!;
+
+        public long RoomId { get; set; }
+        [ForeignKey(nameof(RoomId))]
+        public virtual Room Room { get; set; } = null!;
     }
 }
