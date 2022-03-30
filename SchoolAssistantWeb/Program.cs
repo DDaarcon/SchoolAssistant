2dbf873acef7dca2f0e233896724f11997c67904
DDaarcon <wiktorowski.dariusz1@gmail.com>
2022-03-30 22:32:52 +0200

    Seeding fix

--- SchoolAssistantWeb/Program.cs
+++ SchoolAssistantWeb/Program.cs
@@ -1,7 +1,5 @@
-using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using SchoolAssistant.DAL;
-using SchoolAssistant.DAL.Models;
 using SchoolAssistant.DAL.Models.AppStructure;
 using SchoolAssistant.Infrastructure.InjectablePattern;
 
@@ -13,9 +11,10 @@ builder.Services.AddDbContext<SADbContext>(options =>
     options.UseSqlServer(connectionString));
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
-builder.Services.AddIdentity<User, Role>(options => {
+builder.Services.AddIdentity<User, Role>(options =>
+{
     options.SignIn.RequireConfirmedAccount = true;
-    })
+})
     .AddEntityFrameworkStores<SADbContext>();
 builder.Services.AddRazorPages();
 
@@ -45,7 +44,9 @@ app.UseAuthorization();
 
 app.MapRazorPages();
 
-var seeder = app.Services.GetRequiredService<IDefaultDataSeeder>();
+
+var scopeFactory = app.Services.GetRequiredService<IServiceScopeFactory>();
+var seeder = scopeFactory.CreateScope().ServiceProvider.GetRequiredService<IDefaultDataSeeder>();
 await seeder.SeedAllAsync();
 
 app.Run();
