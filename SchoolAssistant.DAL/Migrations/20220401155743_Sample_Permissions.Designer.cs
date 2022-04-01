e1d752d632dd77166d50ab500351049369f1ed07
DDaarcon <wiktorowski.dariusz1@gmail.com>
2022-04-01 20:25:15 +0200

    Migration

--- SchoolAssistant.DAL/Migrations/20220401155743_Sample_Permissions.Designer.cs
+++ SchoolAssistant.DAL/Migrations/20220401155743_Sample_Permissions.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using SchoolAssistant.DAL;
 
@@ -11,9 +12,10 @@ using SchoolAssistant.DAL;
 namespace SchoolAssistant.DAL.Migrations
 {
     [DbContext(typeof(SADbContext))]
-    partial class SADbContextModelSnapshot : ModelSnapshot
+    [Migration("20220401155743_Sample_Permissions")]
+    partial class Sample_Permissions
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
