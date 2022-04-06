119fc790881dbcd5d981763b0445e8a7ac2ab839
DDaarcon <wiktorowski.dariusz1@gmail.com>
2022-04-06 16:46:57 +0200

    Added Reactjs.Net

--- SchoolAssistantWeb/Program.cs
+++ SchoolAssistantWeb/Program.cs
@@ -1,4 +1,7 @@
+using JavaScriptEngineSwitcher.Extensions.MsDependencyInjection;
+using JavaScriptEngineSwitcher.V8;
 using Microsoft.EntityFrameworkCore;
+using React.AspNet;
 using SchoolAssistant.DAL;
 using SchoolAssistant.DAL.Models.AppStructure;
 using SchoolAssistant.Infrastructure.InjectablePattern;
@@ -11,6 +14,17 @@ var connectionString = builder.Configuration.GetConnectionString("DefaultConnect
 builder.Services.AddDbContext<SADbContext>(options =>
     options.UseSqlServer(connectionString));
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
+
+#endregion
+
+#region ReactJS.NET
+
+builder.Services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
+builder.Services.AddReact();
+
+// Make sure a JS engine is registered, or you will get an error!
+builder.Services.AddJsEngineSwitcher(options => options.DefaultEngineName = V8JsEngine.EngineName)
+  .AddV8();
 
 #endregion
 
@@ -55,6 +69,30 @@ else
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
+
+#region ReactJS.NET pt 2
+
+// Initialise ReactJS.NET. Must be before static files.
+app.UseReact(config =>
+{
+    // If you want to use server-side rendering of React components,
+    // add all the necessary JavaScript files here. This includes
+    // your components as well as all of their dependencies.
+    // See http://reactjs.net/ for more information. Example:
+    config
+      .AddScript("~/js/Schedule/schedule.jsx");
+    //  .AddScript("~/js/Second.jsx");
+
+    // If you use an external build too (for example, Babel, Webpack,
+    // Browserify or Gulp), you can improve performance by disabling
+    // ReactJS.NET's version of Babel and loading the pre-transpiled
+    // scripts. Example:
+    //config
+    //  .SetLoadBabel(false)
+    //  .AddScriptWithoutTransform("~/js/bundle.server.js");
+});
+
+#endregion
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
