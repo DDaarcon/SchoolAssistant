a38ec125534638673c135aca51c38f524006c273
DDaarcon <wiktorowski.dariusz1@gmail.com>
2022-04-29 01:05:06 +0200

    Fetching student entries + modification data

--- SchoolAssistant.Logic/DataManagement/Students/StudentsDataManagementService.cs
+++ SchoolAssistant.Logic/DataManagement/Students/StudentsDataManagementService.cs
@@ -1,4 +1,5 @@
-﻿using SchoolAssistant.DAL.Models.StudentsOrganization;
+﻿using Microsoft.EntityFrameworkCore;
+using SchoolAssistant.DAL.Models.StudentsOrganization;
 using SchoolAssistant.DAL.Models.StudentsParents;
 using SchoolAssistant.DAL.Repositories;
 using SchoolAssistant.Infrastructure.Models.DataManagement.Students;
@@ -18,24 +19,52 @@ namespace SchoolAssistant.Logic.DataManagement.Students
     {
         private readonly IRepository<OrganizationalClass> _orgClassRepo;
         private readonly IRepository<Student> _studentRepo;
+        private readonly IStudentRegisterRecordsDataManagementService _registerDataManagementService;
 
         public StudentsDataManagementService(
             IRepository<OrganizationalClass> orgClassRepo,
-            IRepository<Student> studentRepo)
+            IRepository<Student> studentRepo,
+            IStudentRegisterRecordsDataManagementService registerDataManagementService)
         {
             _orgClassRepo = orgClassRepo;
             _studentRepo = studentRepo;
+            _registerDataManagementService = registerDataManagementService;
         }
 
 
-        public async Task<StudentListEntryJson[]> GetEntriesJsonAsync(long classId)
+        public Task<StudentListEntryJson[]> GetEntriesJsonAsync(long classId)
         {
-            return null;
+            var query = _orgClassRepo.AsQueryable()
+                .Where(x => x.Id == classId)
+                .SelectMany(x => x.Students);
+
+            return query.Select(x => new StudentListEntryJson
+            {
+                id = x.Id,
+                name = x.Info.GetFullName(),
+                numberInJournal = x.NumberInJournal
+            }).ToArrayAsync();
         }
 
         public async Task<StudentModificationDataJson?> GetModificationDataJsonAsync(long id)
         {
-            return null;
+            var student = await _studentRepo.GetByIdAsync(id);
+            if (student is null)
+                return null;
+
+            var registerRecords = await _registerDataManagementService.GetEntriesJsonAsync();
+
+            return new StudentModificationDataJson
+            {
+                data = new StudentDetailsJson
+                {
+                    id = student.Id,
+                    numberInJournal = student.NumberInJournal,
+                    organizationalClassId = student.OrganizationalClassId,
+                    registerRecordId = student.InfoId
+                },
+                registerRecords = registerRecords
+            };
         }
 
         public async Task<ResponseJson> CreateOrUpdateAsync(StudentDetailsJson model)
