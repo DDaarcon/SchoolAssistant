65bed0638b3f965003ecfd460f17a3c993340b58
DDaarcon <wiktorowski.dariusz1@gmail.com>
2022-04-29 05:13:29 +0200

    Students modification component, first steps

--- SchoolAssistantWeb/wwwroot/js/data-management/students.tsx
+++ SchoolAssistantWeb/wwwroot/js/data-management/students.tsx
@@ -3,6 +3,12 @@
     numberInJournal: number;
 }
 
+interface StudentRegisterRecordListEntry {
+    id: number;
+    name: string;
+    className?: string;
+}
+
 interface StudentDetails {
     id?: number;
 
@@ -11,7 +17,7 @@ interface StudentDetails {
 
     organizationalClassId?: number;
 
-    numberInJournal: number;
+    numberInJournal?: number;
 }
 
 interface StudentRegisterRecordDetails {
@@ -26,7 +32,26 @@ interface StudentRegisterRecordDetails {
 
     personalId: string;
     address: string;
+}
 
+interface ParentRegisterSubecordDetails {
+    firstName: string;
+    secondName?: string;
+    lastName: string;
+
+    phoneNumber: string;
+    email?: string;
+
+    address: string;
+}
+
+interface StudentModificationData {
+    data: StudentDetails;
+    registerRecords: StudentRegisterRecordListEntry[];
+}
+
+interface StudentRegisterRecordModificationData {
+    data: StudentRegisterRecordDetails;
 }
 
 
@@ -158,15 +183,138 @@ const StudentsTable = (props: StudentsTableProps) => {
 
 type StudentModificationComponentProps = GroupedModificationComponentProps;
 type StudentModificationComponentState = {
-
+    awaitingData: boolean;
+    data: StudentDetails;
+    registerRecords: StudentRegisterRecordListEntry[];
 }
 class StudentModificationComponent extends React.Component<StudentModificationComponentProps, StudentModificationComponentState> {
+    constructor(props) {
+        super(props);
 
+        this.state = {
+            awaitingData: this.props.recordId > 0,
+            data: {
+                numberInJournal: 0,
+                organizationalClassId: this.props.groupId as number
+            },
+            registerRecords: []
+        }
+
+        if (this.props.recordId)
+            this.fetchAsync();
+        else
+            this.fetchRegisterRecords();
+    }
+
+    private async fetchAsync() {
+        let response = await server.getAsync<StudentModificationData>("StudentModificationData", {
+            id: this.props.recordId
+        });
+
+        this.setState({
+            data: response.data,
+            registerRecords: response.registerRecords,
+            awaitingData: false
+        });
+    }
+
+    private async fetchRegisterRecords() {
+        let response = await server.getAsync<StudentRegisterRecordListEntry[]>("StudentRegisterRecordEntries");
+
+        this.setState({ registerRecords: response, awaitingData: false });
+    }
+
+    createOnTextChangeHandler: (property: keyof StudentDetails) => React.ChangeEventHandler<HTMLInputElement> = (property) => {
+        return (event) => {
+            const value = event.target.value;
+
+            this.setState(prevState => {
+                const data = { ...prevState.data };
+                data[property] = (value as unknown) as never;
+                return { data };
+            });
+
+            this.props.onMadeAnyChange();
+        }
+    }
+
+    onRegisterRecordChangeHandler: React.ChangeEventHandler<HTMLSelectElement> = (event) => {
+        const value = parseInt(event.target.value);
+
+        this.setState(prevState => {
+            const data = { ...prevState.data };
+            data.registerRecordId = value;
+            return { data };
+        });
+
+        this.props.onMadeAnyChange();
+    }
+
+    onSubmitAsync: React.FormEventHandler<HTMLFormElement> = async (event) => {
+        event.preventDefault();
+
+        let response = await server.postAsync<ResponseJson>("StudentData", undefined, {
+            ...this.state.data
+        });
+
+        if (response.success)
+            await this.props.reloadAsync();
+        else
+            console.debug(response);
+    }
 
     render() {
+        if (this.state.awaitingData)
+            return (
+                <Loader
+                    enable={true}
+                    size={LoaderSize.Medium}
+                    type={LoaderType.DivWholeSpace}
+                />
+            )
+
         return (
-            <>
-            </>
+            <div>
+                <form onSubmit={this.onSubmitAsync}>
+                    <div className="form-group">
+                        <label htmlFor="number-in-journal-input">Numer klasy</label>
+                        <input
+                            type="number"
+                            className="form-control"
+                            name="number-in-journal-input"
+                            value={this.state.data.numberInJournal}
+                            onChange={this.createOnTextChangeHandler('numberInJournal')}
+                        />
+                    </div>
+
+                    <div className="form-group">
+                        <label htmlFor="register-record-input">Dane ucznia</label>
+                        <select
+                            className="form-select"
+                            multiple
+                            name="register-record-input"
+                            value={this.state.data.registerRecordId}
+                            onChange={this.onRegisterRecordChangeHandler}
+                        >
+                            {this.state.registerRecords.map(x =>
+                                <option key={x.id}
+                                    value={x.id}
+                                >
+                                    {x.name}
+                                </option>
+                            )}
+                        </select>
+                    </div>
+
+                    <div className="form-group">
+                        <input
+                            type="submit"
+                            value="Zapisz"
+                            className="form-control"
+                        />
+                    </div>
+                </form>
+            </div>
         )
     }
 }
