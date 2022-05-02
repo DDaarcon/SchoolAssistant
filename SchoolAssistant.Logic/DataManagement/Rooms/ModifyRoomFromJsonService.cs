41ea7be3d5aae4855bbcab9ae2749d12093efcee
DDaarcon <wiktorowski.dariusz1@gmail.com>
2022-05-03 00:16:08 +0200

    Draft of room management services

---
+++ SchoolAssistant.Logic/DataManagement/Rooms/ModifyRoomFromJsonService.cs
@@ -0,0 +1,29 @@
+﻿using SchoolAssistant.DAL.Models.Rooms;
+using SchoolAssistant.DAL.Repositories;
+using SchoolAssistant.Infrastructure.Models.DataManagement.Rooms;
+using SchoolAssistant.Infrastructure.Models.Shared.Json;
+
+namespace SchoolAssistant.Logic.DataManagement.Rooms
+{
+    public interface IModifyRoomFromJsonService
+    {
+        Task<ResponseJson> CreateOrUpdateAsync(RoomDetailsJson model);
+    }
+
+    [Injectable]
+    public class ModifyRoomFromJsonService : IModifyRoomFromJsonService
+    {
+        private readonly IRepository<Room> _roomRepo;
+
+        public ModifyRoomFromJsonService(
+            IRepository<Room> roomRepo)
+        {
+            _roomRepo = roomRepo;
+        }
+
+        public Task<ResponseJson> CreateOrUpdateAsync(RoomDetailsJson model)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ SchoolAssistant.Logic/DataManagement/Rooms/RoomDataManagementService.cs
@@ -0,0 +1,46 @@
+﻿using SchoolAssistant.DAL.Models.Rooms;
+using SchoolAssistant.DAL.Repositories;
+using SchoolAssistant.Infrastructure.Models.DataManagement.Rooms;
+using SchoolAssistant.Infrastructure.Models.Shared.Json;
+
+namespace SchoolAssistant.Logic.DataManagement.Rooms
+{
+    public interface IRoomDataManagementService
+    {
+        Task<ResponseJson> CreateOrUpdateAsync(RoomDetailsJson model);
+        Task<RoomListEntryJson[]> GetEntriesJsonAsync();
+        Task<RoomModificationDataJson?> GetModificationDataJsonAsync(long id);
+        Task<string> GetDefaultNameAsync();
+    }
+
+    [Injectable]
+    public class RoomDataManagementService : IRoomDataManagementService
+    {
+        private readonly IRepository<Room> _roomRepo;
+
+        public RoomDataManagementService(
+            IRepository<Room> roomRepo)
+        {
+            _roomRepo = roomRepo;
+        }
+
+        public Task<string> GetDefaultNameAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<RoomListEntryJson[]> GetEntriesJsonAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<RoomModificationDataJson?> GetModificationDataJsonAsync(long id)
+        {
+            throw new NotImplementedException();
+        }
+        public Task<ResponseJson> CreateOrUpdateAsync(RoomDetailsJson model)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
