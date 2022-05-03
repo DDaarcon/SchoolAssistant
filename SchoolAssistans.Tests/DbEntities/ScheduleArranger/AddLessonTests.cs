62bdd01d410341846acbbd62c14ef0b8a67e8f4b
DDaarcon <wiktorowski.dariusz1@gmail.com>
2022-05-03 05:52:13 +0200

    Schedule Arranger: add lesson service - more tests

--- SchoolAssistans.Tests/DbEntities/ScheduleArranger/AddLessonTests.cs
+++ SchoolAssistans.Tests/DbEntities/ScheduleArranger/AddLessonTests.cs
@@ -3,6 +3,7 @@ using SchoolAssistant.DAL.Models.Lessons;
 using SchoolAssistant.DAL.Models.Rooms;
 using SchoolAssistant.DAL.Models.Staff;
 using SchoolAssistant.DAL.Models.StudentsOrganization;
+using SchoolAssistant.DAL.Models.Subjects;
 using SchoolAssistant.DAL.Repositories;
 using SchoolAssistant.Infrastructure.Models.ScheduleArranger;
 using SchoolAssistant.Logic;
@@ -10,7 +11,6 @@ using SchoolAssistant.Logic.ScheduleArranger;
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 
 namespace SchoolAssistans.Tests.DbEntities.ScheduleArranger
@@ -19,6 +19,7 @@ namespace SchoolAssistans.Tests.DbEntities.ScheduleArranger
     {
         private IAddLessonService _addLessonService = null!;
 
+        private IAppConfigRepository _configRepo = null!;
         private IRepository<OrganizationalClass> _orgClassRepo = null!;
         private IRepository<Teacher> _teacherRepo = null!;
         private IRepository<Room> _roomRepo = null!;
@@ -47,11 +48,17 @@ namespace SchoolAssistans.Tests.DbEntities.ScheduleArranger
 
         protected override void SetupServices()
         {
+            _configRepo = new AppConfigRepository(_Context, null);
             _orgClassRepo = new Repository<OrganizationalClass>(_Context, null);
             _teacherRepo = new Repository<Teacher>(_Context, null);
             _roomRepo = new Repository<Room>(_Context, null);
 
-            _addLessonService = new AddLessonService();
+            _addLessonService = new AddLessonService(
+                _configRepo,
+                _orgClassRepo,
+                new Repository<Subject>(_Context, null),
+                _teacherRepo,
+                _roomRepo);
         }
 
 
@@ -70,7 +77,8 @@ namespace SchoolAssistans.Tests.DbEntities.ScheduleArranger
                 {
                     hour = 10,
                     minutes = 20
-                }
+                },
+                day = DayOfWeek.Tuesday
             };
 
             var res = await _addLessonService.AddToClass(model);
@@ -86,5 +94,265 @@ namespace SchoolAssistans.Tests.DbEntities.ScheduleArranger
             Assert.AreEqual(res!.lesson!.subject.id, model.subjectId);
             Assert.AreEqual(res!.lesson!.room.id, model.roomId);
         }
+
+        [Test]
+        public async Task Should_add_lesson_custom_duration()
+        {
+            var subject = _Teacher.SubjectOperations.MainIter.First();
+            var model = new AddLessonRequestJson
+            {
+                classId = _classWithNoSchedule.Id,
+                customDuration = 90,
+                lecturerId = _Teacher.Id,
+                subjectId = subject.Id,
+                roomId = _Room.Id,
+                time = new TimeJson
+                {
+                    hour = 10,
+                    minutes = 20
+                },
+                day = DayOfWeek.Tuesday
+            };
+
+            var res = await _addLessonService.AddToClass(model);
+
+            AssertResponseSuccess(res);
+
+            Assert.AreEqual(res!.lesson!.time, model.time);
+            Assert.AreEqual(res!.lesson!.customDuration, 90);
+            Assert.AreEqual(res!.lesson!.lecturer.name, _Teacher.GetShortenedName());
+            Assert.AreEqual(res!.lesson!.subject.name, subject.Name);
+            Assert.AreEqual(res!.lesson!.room.name, _Room.DisplayName);
+            Assert.AreEqual(res!.lesson!.lecturer.id, model.lecturerId);
+            Assert.AreEqual(res!.lesson!.subject.id, model.subjectId);
+            Assert.AreEqual(res!.lesson!.room.id, model.roomId);
+        }
+
+
+        #region Fails
+
+        [Test]
+        public async Task Should_fail_invalid_hour_to_early()
+        {
+            await _configRepo.ScheduleStartHour.SetAndSaveAsync("11");
+
+            var subject = _Teacher.SubjectOperations.MainIter.First();
+            var model = new AddLessonRequestJson
+            {
+                classId = _classWithNoSchedule.Id,
+                lecturerId = _Teacher.Id,
+                subjectId = subject.Id,
+                roomId = _Room.Id,
+                time = new TimeJson
+                {
+                    hour = 10,
+                    minutes = 20
+                },
+                day = DayOfWeek.Tuesday
+            };
+
+            var res = await _addLessonService.AddToClass(model);
+
+            AssertResponseFail(res);
+        }
+        [Test]
+        public async Task Should_fail_invalid_hour_to_late()
+        {
+            await _configRepo.ScheduleEndhour.SetAndSaveAsync("18");
+
+            var subject = _Teacher.SubjectOperations.MainIter.First();
+            var model = new AddLessonRequestJson
+            {
+                classId = _classWithNoSchedule.Id,
+                lecturerId = _Teacher.Id,
+                subjectId = subject.Id,
+                roomId = _Room.Id,
+                time = new TimeJson
+                {
+                    hour = 17,
+                    minutes = 20
+                },
+                day = DayOfWeek.Tuesday
+            };
+
+            var res = await _addLessonService.AddToClass(model);
+
+            AssertResponseFail(res);
+        }
+        [Test]
+        public async Task Should_fail_invalid_hour()
+        {
+            var subject = _Teacher.SubjectOperations.MainIter.First();
+            var model = new AddLessonRequestJson
+            {
+                classId = _classWithNoSchedule.Id,
+                lecturerId = _Teacher.Id,
+                subjectId = subject.Id,
+                roomId = _Room.Id,
+                time = new TimeJson
+                {
+                    hour = -1,
+                    minutes = 20
+                },
+                day = DayOfWeek.Tuesday
+            };
+
+            var res = await _addLessonService.AddToClass(model);
+
+            AssertResponseFail(res);
+        }
+        [Test]
+        public async Task Should_fail_invalid_minutes()
+        {
+            var subject = _Teacher.SubjectOperations.MainIter.First();
+            var model = new AddLessonRequestJson
+            {
+                classId = _classWithNoSchedule.Id,
+                lecturerId = _Teacher.Id,
+                subjectId = subject.Id,
+                roomId = _Room.Id,
+                time = new TimeJson
+                {
+                    hour = 10,
+                    minutes = 61
+                },
+                day = DayOfWeek.Tuesday
+            };
+
+            var res = await _addLessonService.AddToClass(model);
+
+            AssertResponseFail(res);
+        }
+        [Test]
+        public async Task Should_fail_invalid_classId()
+        {
+            var subject = _Teacher.SubjectOperations.MainIter.First();
+            var model = new AddLessonRequestJson
+            {
+                classId = 9999,
+                lecturerId = _Teacher.Id,
+                subjectId = subject.Id,
+                roomId = _Room.Id,
+                time = new TimeJson
+                {
+                    hour = 10,
+                    minutes = 20
+                },
+                day = DayOfWeek.Tuesday
+            };
+
+            var res = await _addLessonService.AddToClass(model);
+
+            AssertResponseFail(res);
+        }
+        [Test]
+        public async Task Should_fail_invalid_lecturer()
+        {
+            var subject = _Teacher.SubjectOperations.MainIter.First();
+            var model = new AddLessonRequestJson
+            {
+                classId = _classWithNoSchedule.Id,
+                lecturerId = 9999,
+                subjectId = subject.Id,
+                roomId = _Room.Id,
+                time = new TimeJson
+                {
+                    hour = 10,
+                    minutes = 20
+                },
+                day = DayOfWeek.Tuesday
+            };
+
+            var res = await _addLessonService.AddToClass(model);
+
+            AssertResponseFail(res);
+        }
+        [Test]
+        public async Task Should_fail_invalid_subject()
+        {
+            var subject = _Teacher.SubjectOperations.MainIter.First();
+            var model = new AddLessonRequestJson
+            {
+                classId = _classWithNoSchedule.Id,
+                lecturerId = _Teacher.Id,
+                subjectId = 9999,
+                roomId = _Room.Id,
+                time = new TimeJson
+                {
+                    hour = 10,
+                    minutes = 20
+                },
+                day = DayOfWeek.Tuesday
+            };
+
+            var res = await _addLessonService.AddToClass(model);
+
+            AssertResponseFail(res);
+        }
+        [Test]
+        public async Task Should_fail_invalid_room()
+        {
+            var subject = _Teacher.SubjectOperations.MainIter.First();
+            var model = new AddLessonRequestJson
+            {
+                classId = _classWithNoSchedule.Id,
+                lecturerId = _Teacher.Id,
+                subjectId = subject.Id,
+                roomId = 9999,
+                time = new TimeJson
+                {
+                    hour = 10,
+                    minutes = 20
+                },
+                day = DayOfWeek.Tuesday
+            };
+
+            var res = await _addLessonService.AddToClass(model);
+
+            AssertResponseFail(res);
+        }
+        [Test]
+        public async Task Should_fail_missing_time()
+        {
+            var subject = _Teacher.SubjectOperations.MainIter.First();
+            var model = new AddLessonRequestJson
+            {
+                classId = _classWithNoSchedule.Id,
+                lecturerId = _Teacher.Id,
+                subjectId = subject.Id,
+                roomId = _Room.Id,
+                time = null!,
+                day = DayOfWeek.Tuesday
+            };
+
+            var res = await _addLessonService.AddToClass(model);
+
+            AssertResponseFail(res);
+        }
+        [Test]
+        public async Task Should_fail_invalid_day()
+        {
+            var subject = _Teacher.SubjectOperations.MainIter.First();
+            var model = new AddLessonRequestJson
+            {
+                classId = _classWithNoSchedule.Id,
+                lecturerId = _Teacher.Id,
+                subjectId = subject.Id,
+                roomId = _Room.Id,
+                time = new TimeJson
+                {
+                    hour = 10,
+                    minutes = 20
+                },
+                day = (DayOfWeek)10
+            };
+
+            var res = await _addLessonService.AddToClass(model);
+
+            AssertResponseFail(res);
+        }
+
+
+        #endregion
     }
 }
