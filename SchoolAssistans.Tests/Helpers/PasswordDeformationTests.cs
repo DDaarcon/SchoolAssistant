6aa71ca6939570fe57cda0749faa549d4fc602e8
DDaarcon <wiktorowski.dariusz1@gmail.com>
2022-05-27 02:49:21 +0200

    String scrambling

---
+++ SchoolAssistans.Tests/Helpers/PasswordDeformationTests.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.Extensions.Configuration;
+using NUnit.Framework;
+using SchoolAssistant.Logic.General.Other;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SchoolAssistans.Tests.Helpers
+{
+    public class PasswordDeformationTests
+    {
+        private readonly IPasswordDeformationService _deformationSvc;
+
+        public PasswordDeformationTests()
+        {
+            _deformationSvc = new PasswordDeformationService(null);
+        }
+
+
+        [Test]
+        public void Should_deform_and_parse()
+        {
+            string text = "Ala ma kota, kota małego";
+
+            string deformed = _deformationSvc.GetDeformed(text);
+            string readable = _deformationSvc.GetReadable(deformed);
+
+            Assert.AreEqual(text, readable);
+        }
+    }
+}
---
+++ SchoolAssistant.Logic/General/Other/PasswordDeformationService.cs
@@ -0,0 +1,81 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SchoolAssistant.Logic.General.Other
+{
+    public interface IPasswordDeformationService
+    {
+        string GetDeformed(string password);
+        string GetReadable(string deformed);
+    }
+
+    [Injectable]
+    public class PasswordDeformationService : IPasswordDeformationService
+    {
+        private readonly IConfiguration? _config;
+
+        private readonly int _increaseBy;
+        private readonly int _shiftBytesBy;
+        private readonly int _decreaseBy;
+
+        public PasswordDeformationService(
+            IConfiguration? config)
+        {
+            _config = config;
+
+            var passwordDefSec = _config?.GetSection("PasswordDeformation");
+
+            _increaseBy = passwordDefSec?.GetValue<int>("increaseCharBy") ?? 10;
+            _shiftBytesBy = passwordDefSec?.GetValue<int>("shiftCharBytesBy") ?? 3;
+            _decreaseBy = passwordDefSec?.GetValue<int>("decreaseCharBy") ?? 15;
+        }
+
+        public string GetDeformed(string password)
+        {
+            return TransformString(password, false);
+        }
+
+        public string GetReadable(string deformed)
+        {
+            return TransformString(deformed, true);
+        }
+
+        private string TransformString(string text, bool rev)
+        {
+            int multipl = rev ? -1 : 1;
+
+            var increased = ForEach(text.ToCharArray(), (c, index) => (char)(c + (char)_increaseBy * multipl));
+
+            var shifting = ForEach(increased, (c, index) =>
+            {
+                // setting bytes of [index] to [index + shiftCIndex] and reversing them
+                var thisCBytes = BitConverter.GetBytes(c);
+                int shiftCIndex = (index + _shiftBytesBy * multipl) % increased.Length;
+                if (shiftCIndex < 0) shiftCIndex = increased.Length + shiftCIndex;
+                var shiftCBytes = BitConverter.GetBytes(increased[shiftCIndex]);
+
+                return BitConverter.ToChar(new byte[]
+                {
+                    shiftCBytes[1],
+                    shiftCBytes[0]
+                });
+            });
+
+            var decreased = ForEach(text.ToCharArray(), (c, index) => (char)(c - (char)_decreaseBy * multipl));
+
+            return new string(decreased);
+        }
+
+        private char[] ForEach(char[] text, Func<char,int, char> forEach)
+        {
+            var chars = new char[text.Length];
+            for (int i = 0; i < text.Length; i++)
+                chars[i] = forEach.Invoke(text[i], i);
+            return chars;
+        }
+    }
+}
