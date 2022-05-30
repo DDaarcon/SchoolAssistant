7e515c64d40c3d9bfa1f172f2be8e2e1e5c3ed96
DDaarcon <wiktorowski.dariusz1@gmail.com>
2022-05-30 08:08:43 +0200

    Removed commented code

--- SchoolAssistantWeb/React/schedule-display/components/lesson-tiles/teacher-lesson-tile.tsx
+++ SchoolAssistantWeb/React/schedule-display/components/lesson-tiles/teacher-lesson-tile.tsx
@@ -40,20 +40,6 @@ export default class TeacherLessonTile extends LessonTile {
                     />
                 </div>
             </div>
-            //<>
-            //    <div className="sched-teac-lesson-time">
-            //        {displayTime(this.props.lesson.time)}
-            //    </div>
-            //    <div className="sched-teac-lesson-subject">
-            //        {this.props.lesson.subject.name}
-            //    </div>
-            //    <div className="sched-teac-lesson-lecturer">
-            //        {this.props.lesson.lecturer.name}
-            //    </div>
-            //    <div className="sched-teac-lesson-room">
-            //        {this.props.lesson.room.name}
-            //    </div>
-            //</>
         )
     }
 }
