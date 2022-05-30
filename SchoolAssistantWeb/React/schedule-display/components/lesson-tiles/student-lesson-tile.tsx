b61164ba4bff7d2955556229ad74b9465047a969
DDaarcon <wiktorowski.dariusz1@gmail.com>
2022-05-30 05:26:58 +0200

    Schedule Display styles

--- SchoolAssistantWeb/React/schedule-display/components/lesson-tiles/student-lesson-tile.tsx
+++ SchoolAssistantWeb/React/schedule-display/components/lesson-tiles/student-lesson-tile.tsx
@@ -1,23 +1,58 @@
 ﻿import React from "react";
 import { displayTime } from "../../../schedule-shared/help/time-functions";
+import { LabelValue } from "../../../shared/form-controls";
 import LessonTile from "./lesson-tile";
 import './student-lesson-tile.css';
 
 export default class StudentLessonTile extends LessonTile {
     protected getInnerComponents(): JSX.Element {
+        const labelValueStyle: React.CSSProperties = {
+            width: '100%',
+            fontSize: '0.8em',
+            marginBottom: 0
+        };
+
+        const labelStyle: React.CSSProperties = {
+            width: '20%',
+        };
+
+
         return (
             <div className="sched-stud-lesson-inner-container">
-                <div className={"sched-stud-lesson-time " + (this.state.hover ? "expanded" : "")}>
-                    {displayTime(this.props.lesson.time)}
-                </div>
+
                 <div className={"sched-stud-lesson-subject"}>
                     {this.props.lesson.subject.name}
                 </div>
-                <div className={"sched-stud-lesson-lecturer " + (this.state.hover ? "expanded" : "")}>
-                    {this.props.lesson.lecturer.name}
-                </div>
-                <div className={"sched-stud-lesson-room " + (this.state.hover ? "expanded" : "")}>
-                    {this.props.lesson.room.name}
+
+                <div className={"sched-lesson-expandable " + (this.state.hover ? "expanded" : "")}>
+                    <LabelValue
+                        label="czas"
+                        valueComp={displayTime(this.props.lesson.time)}
+                        outerStyle={labelValueStyle}
+                        labelOuterStyle={labelStyle}
+                    />
+                    <LabelValue
+                        label="miejsce"
+                        valueComp={this.props.lesson.room.name}
+                        outerStyle={labelValueStyle}
+                        labelOuterStyle={labelStyle}
+                    />
+                    <LabelValue
+                        label="wykł."
+                        valueComp={this.props.lesson.lecturer.name}
+                        outerStyle={labelValueStyle}
+                        labelOuterStyle={labelStyle}
+                    />
+
+                    {/*<div className={"sched-stud-lesson-lecturer " + (this.state.hover ? "expanded" : "")}>*/}
+                    {/*    {this.props.lesson.lecturer.name}*/}
+                    {/*</div>*/}
+                    {/*<div className={"sched-stud-lesson-room " + (this.state.hover ? "expanded" : "")}>*/}
+                    {/*    {this.props.lesson.room.name}*/}
+                    {/*</div>*/}
+                    {/*<div className={"sched-stud-lesson-time " + (this.state.hover ? "expanded" : "")}>*/}
+                    {/*    {displayTime(this.props.lesson.time)}*/}
+                    {/*</div>*/}
                 </div>
             </div>
         )
