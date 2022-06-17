d816ae49aa1f90c9845fc1cca48d83e54abfed50
DDaarcon <wiktorowski.dariusz1@gmail.com>
2022-06-17 06:20:41 +0200

    made load newer lessons buttons also pretty

--- SchoolAssistantWeb/React/scheduled-lessons-list/components-controls/load-lessons-buttons/load-lessons-button-icon.tsx
+++ SchoolAssistantWeb/React/scheduled-lessons-list/components-controls/load-lessons-buttons/load-lessons-button-icon.tsx
@@ -34,7 +34,7 @@ export default class LoadLessonsButtonIcon extends React.Component<LoadLessonsBu
 
     render() {
         return (
-            <div className="all-load-button-icon-box">
+            <div className={this._boxClassName}>
                 <div className={this._className}
                     style={{
                         animationDuration: `${this._speed}s`,
@@ -54,10 +54,17 @@ export default class LoadLessonsButtonIcon extends React.Component<LoadLessonsBu
         }
     }
 
+    private get _boxClassName() {
+        return "all-load-button-icon-box " + enumAssignSwitch<string, typeof LoadLessonsButtonLayout>(LoadLessonsButtonLayout, this.props.layout, {
+            Upright: () => "all-load-button-icon-box-upright",
+            UpsideDown: () => "all-load-button-icon-box-upside-down"
+        })
+    }
+
     private get _className() {
-        return enumAssignSwitch<string, typeof LoadLessonsButtonLayout>(LoadLessonsButtonLayout, this.props.layout, {
-            Upright: () => "sll-load-button-icon sll-load-button-icon-upright",
-            UpsideDown: () => "sll-load-button-icon sll-load-button-icon-upside-down"
+        return "sll-load-button-icon " + enumAssignSwitch<string, typeof LoadLessonsButtonLayout>(LoadLessonsButtonLayout, this.props.layout, {
+            Upright: () => "sll-load-button-icon-upright",
+            UpsideDown: () => "sll-load-button-icon-upside-down"
         })
     }   
 }
