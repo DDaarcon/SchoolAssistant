f0878c77f9a553c673f37c5f731d8d896bea95bd
DDaarcon <wiktorowski.dariusz1@gmail.com>
2022-06-20 11:06:16 +0200

    text input component

--- SchoolAssistantWeb/React/shared/form-controls.ts
+++ SchoolAssistantWeb/React/shared/form-controls.ts
@@ -3,9 +3,11 @@ import Select, { Option, OnChangeHandler, OnChangeIdHandler } from './form-contr
 import Multiselect from './form-controls/multiselect';
 import './form-controls/form-controls.css';
 import LabelValue from './form-controls/label-value';
+import TextInput from './form-controls/text-input';
 
 export {
     Input,
+    TextInput,
     Select,
     Multiselect,
     LabelValue,
---
+++ SchoolAssistantWeb/React/shared/form-controls/text-input.tsx
@@ -0,0 +1,27 @@
+﻿import React from "react";
+import Input from "./input";
+
+type TextInputProps = {
+    label: string;
+    name: string;
+    value?: string | number;
+    onChange: (value: string) => void;
+    hasErrors?: boolean;
+    errorMessages?: string[];
+    disabled?: boolean;
+    containerClassName?: string;
+    inputClassName?: string;
+    placeholder?: string;
+}
+
+const TextInput = (props: TextInputProps) => {
+    const { onChange, ...reducedProps } = props;
+    return (
+        <Input
+            {...reducedProps}
+            type="text"
+            onChange={(ev) => props.onChange(ev.target.value)}
+        />
+    );
+}
+export default TextInput;
