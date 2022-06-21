897e491aad30c4e9b888f47f6ba88bc4ee376801
DDaarcon <wiktorowski.dariusz1@gmail.com>
2022-06-21 22:25:48 +0200

    panel anchor + some styles

--- SchoolAssistantWeb/React/lesson-conduction-panel/panel/components/anchor.tsx
+++ SchoolAssistantWeb/React/lesson-conduction-panel/panel/components/anchor.tsx
@@ -1,4 +1,6 @@
 ﻿import React from "react";
+import LessonTimerService from "../../services/lesson-timer-service";
+import StoreAndSaveService from "../../services/store-and-save-service";
 import TogglePanelService from "../../services/toggle-panel-service";
 import './anchor.css';
 
@@ -13,9 +15,38 @@ export default class Anchor extends React.Component<AnchorProps, AnchorState> {
             <button className="lcp-anchor"
                 onClick={this.click}
             >
-
+                <div>
+                    <span>Zajęcia</span>
+                    {this.timeComponent()}
+                </div>
             </button>
         )
+    }
+
+    private timeComponent() {
+        if (LessonTimerService.isSetUp)
+            return (
+                <span>
+                    {LessonTimerService.minutes}:{this.displaySeconds(LessonTimerService.seconds)}
+                </span>
+            );
+        return <></>
+    }
+
+    private displaySeconds(seconds: number) {
+        if (seconds < 10)
+            return '0' + seconds.toString();
+        return seconds.toString();
+    }
+
+    componentDidMount() {
+        if (!LessonTimerService.isSetUp)
+            LessonTimerService.setUp(
+                StoreAndSaveService.startTime,
+                StoreAndSaveService.duration
+            );
+
+        LessonTimerService.onUpdate(() => this.forceUpdate());
     }
 
     private click = () => {
