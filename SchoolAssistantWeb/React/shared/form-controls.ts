8bba9641eb482b4c64f31327ce9959fa7e61b3e3
DDaarcon <wiktorowski.dariusz1@gmail.com>
2022-06-22 20:55:05 +0200

    textarea component

--- SchoolAssistantWeb/React/shared/form-controls.ts
+++ SchoolAssistantWeb/React/shared/form-controls.ts
@@ -4,12 +4,14 @@ import Multiselect from './form-controls/multiselect';
 import './form-controls/form-controls.css';
 import LabelValue from './form-controls/label-value';
 import TextInput from './form-controls/text-input';
+import TextArea from './form-controls/text-area';
 
 export {
     Input,
     TextInput,
     Select,
     Multiselect,
+    TextArea,
     LabelValue,
     Option,
     OnChangeHandler,
---
+++ SchoolAssistantWeb/React/shared/form-controls/text-area.tsx
@@ -0,0 +1,53 @@
+﻿import React from 'react';
+import defaultErrorMessage from './default-error-msg';
+
+type TextAreaProps = {
+    label?: string;
+    name: string;
+    value?: string;
+    onChange: (value: string) => void;
+
+    hasErrors?: boolean;
+    errorMessages?: string[];
+    warningMessages?: string[];
+
+    disabled?: boolean;
+    containerClassName?: string;
+    inputClassName?: string;
+    placeholder?: string;
+}
+const TextArea = (props: TextAreaProps) => {
+    const hasErrors = props.hasErrors ?? props.errorMessages?.length ?? false;
+    const hasWarnings = props.warningMessages?.length;
+
+    const label = props.label?.length
+        ? <label htmlFor={props.name}>{props.label}</label>
+        : <></>;
+
+
+    return (
+        <div className={"form-group my-form-group " + (props.containerClassName ?? '')}>
+            {label}
+
+            <textarea
+                className={"form-control" + (hasErrors ? ' is-invalid' : '') + " " + (props.inputClassName ?? '')}
+                name={props.name}
+                value={props.value ?? ""}
+                onChange={ev => props.onChange(ev.target.value)}
+                disabled={props.disabled}
+                placeholder={props.placeholder}
+            />
+
+            {hasErrors ? (
+                <div className="invalid-feedback">
+                    {props.errorMessages?.map(x => defaultErrorMessage(x)).join(' ')}
+                </div>
+            ) : hasWarnings ? (
+                <div className="invalid-feedback warning-msg">
+                    {props.warningMessages?.join(' ')}
+                </div>
+            ) : undefined}
+        </div>
+    )
+}
+export default TextArea;
