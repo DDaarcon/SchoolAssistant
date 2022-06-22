00d9a4b5168cd237b7eb630e6191ed8ccc4c500b
DDaarcon <wiktorowski.dariusz1@gmail.com>
2022-06-22 20:24:38 +0200

    styled student selection

--- SchoolAssistantWeb/React/lesson-conduction-panel/panel/panel-content-area/giving-mark-page/giving-mark-page.tsx
+++ SchoolAssistantWeb/React/lesson-conduction-panel/panel/panel-content-area/giving-mark-page/giving-mark-page.tsx
@@ -1,7 +1,10 @@
 ﻿import React from "react";
 import ModCompBase from "../../../../shared/form-controls/mod-comp-base";
 import MarkInput from "../../../marks/mark-input";
+import StoreAndSaveService from "../../../services/store-and-save-service";
 import GiveMarkModel from "./give-mark-model";
+import StudentSelectionEntry from "./student-selection-entry";
+import './giving-mark-page.css';
 
 type GivingMarkPageProps = {}
 type GivingMarkPageState = {
@@ -45,13 +48,27 @@ export default class GivingMarkPage extends ModCompBase<GiveMarkModel, GivingMar
 
     render() {
         return (
-            <div className="giving-mark-details">
-                <MarkInput
-                    mark={this.state.data.mark}
-                    onChange={mark => this.setStateFnData(data => data.mark = mark)}
-                    errorMessages={this._validator.getErrorMsgsFor('mark')}
-                    warningMessages={this._validator.getWarningMsgsFor('mark')}
-                />
+            <div className="giving-mark-page">
+
+                <div className="giving-mark-details">
+                    <MarkInput
+                        mark={this.state.data.mark}
+                        onChange={mark => this.setStateFnData(data => data.mark = mark)}
+                        errorMessages={this._validator.getErrorMsgsFor('mark')}
+                        warningMessages={this._validator.getWarningMsgsFor('mark')}
+                    />
+                </div>
+
+                <div className="student-selection-list">
+                    {StoreAndSaveService.students.map(student => (
+                        <StudentSelectionEntry
+                            key={student.id}
+                            selected={this.state.data.studentId == student.id}
+                            select={studentId => this.setStateFnData(data => data.studentId = studentId)}
+                            {...student}
+                        />
+                    ))}
+                </div>
             </div>
         )
     }
---
+++ SchoolAssistantWeb/React/lesson-conduction-panel/panel/panel-content-area/giving-mark-page/student-selection-entry.tsx
@@ -0,0 +1,27 @@
+﻿import React from "react";
+import ParticipatingStudentModel from "../../../interfaces/participating-student-model";
+import './student-selection-entry.css';
+
+type StudentSelectionEntryProps = ParticipatingStudentModel & {
+    selected: boolean;
+    select: (studentId: number) => void;
+}
+type StudentSelectionEntryState = {}
+
+export default class StudentSelectionEntry extends React.Component<StudentSelectionEntryProps, StudentSelectionEntryState> {
+
+    render() {
+        return (
+            <button className={"student-selection-entry " + (this.props.selected ? "student-selection-entry-selected" : "")}
+                onClick={() => this.props.select(this.props.id)}
+            >
+                <span>
+                    {this.props.numberInJournal}
+                </span>
+                <span>
+                    {this.props.lastName} {this.props.firstName}
+                </span>
+            </button>
+        )
+    }
+}
