c8d2bc90b37b1eb44136c71e3167e9ee6dff8d6c
DDaarcon <wiktorowski.dariusz1@gmail.com>
2022-06-22 09:07:02 +0200

    simple giving mark page

---
+++ SchoolAssistantWeb/React/lesson-conduction-panel/marks/mark-input.tsx
@@ -0,0 +1,37 @@
+﻿import React from "react";
+import { Option, Select, TextInput } from "../../shared/form-controls";
+
+type MarkInputProps = {
+    prefix: '' | '-' | '+';
+
+}
+
+export default class MarkInput extends React.Component<MarkInputProps> {
+
+    render() {
+        return (
+            <>
+                <Select
+                    name="mark-prefix-input"
+                    options={this._prefixOptions}
+                    containerClassName="mark-prefix-input-container"
+                    inputClassName="mark-prefix-input"
+                    onChangeId={ }
+                />
+                <TextInput
+                    name="mark-input"
+                    containerClassName="mark-input-container"
+                    inputClassName="mark-input"
+                    value={ }
+                    onChange={ }
+                />
+            </>
+        )
+    }
+
+    private _prefixOptions: Option<string>[] = [
+        { label: '', value: '' },
+        { label: '-', value: '-' },
+        { label: '+', value: '+' }
+    ];
+}
---
+++ SchoolAssistantWeb/React/lesson-conduction-panel/panel/panel-content-area/giving-mark-page/giving-mark-page.tsx
@@ -0,0 +1,19 @@
+﻿import React from "react";
+import MarkInput from "../../../marks/mark-input";
+
+type GivingMarkPageProps = {}
+type GivingMarkPageState = {}
+
+export default class GivingMarkPage extends React.Component<GivingMarkPageProps, GivingMarkPageState> {
+
+    render() {
+        return (
+            <div className="giving-mark-details">
+                <MarkInput
+                    prefix=""
+
+                />
+            </div>
+        )
+    }
+}
