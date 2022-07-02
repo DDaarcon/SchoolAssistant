b78cf4e3c06cead24b25e5a6917bf6c0d507df35
DDaarcon <wiktorowski.dariusz1@gmail.com>
2022-07-02 14:56:54 +0200

    Login page cleanup

--- SchoolAssistantWeb/Areas/Identity/Pages/Account/Login.cshtml.cs
+++ SchoolAssistantWeb/Areas/Identity/Pages/Account/Login.cshtml.cs
@@ -59,7 +59,8 @@ namespace SchoolAssistant.Web.Areas.Identity.Pages.Account
             ///     directly from your code. This API may change or be removed in future releases.
             /// </summary>
             [Required]
-            public string Email { get; set; }
+            [Display(Name = "Nazwa użytkownika")]
+            public string UserName { get; set; }
 
             /// <summary>
             ///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
@@ -67,6 +68,7 @@ namespace SchoolAssistant.Web.Areas.Identity.Pages.Account
             /// </summary>
             [Required]
             [DataType(DataType.Password)]
+            [Display(Name = "Hasło")]
             public string Password { get; set; }
 
             /// <summary>
@@ -104,7 +106,7 @@ namespace SchoolAssistant.Web.Areas.Identity.Pages.Account
             {
                 // This doesn't count login failures towards account lockout
                 // To enable password failures to trigger account lockout, set lockoutOnFailure: true
-                var result = await _signInManager.PasswordSignInAsync(Input.Email, Input.Password, Input.RememberMe, lockoutOnFailure: false).ConfigureAwait(false);
+                var result = await _signInManager.PasswordSignInAsync(Input.UserName, Input.Password, Input.RememberMe, lockoutOnFailure: false).ConfigureAwait(false);
                 if (result.Succeeded)
                 {
                     _logger.LogInformation("User logged in.");
